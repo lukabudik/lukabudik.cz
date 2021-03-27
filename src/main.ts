60fc443637c40ef2df24a118605efd5b9a44177f
Luka Budík <sheffikthegod@gmail.com>
2021-03-27 22:07:50 +0100

    fix: plausible analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { setupLayouts } from 'layouts-generated'
-import { VuePlausible } from 'vue-plausible'
 // eslint-disable-next-line prettier/prettier
 import 'windi.css'
 import NProgress from 'nprogress'
@@ -41,4 +40,4 @@ const app = createApp(App)
 app.use(i18n)
 app.use(router)
 app.mount('#app')
-app.use(VuePlausible, {domain: "budik.me"})
+
